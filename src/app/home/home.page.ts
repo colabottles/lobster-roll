a6d79a3e3fa8822b36b627618cdb041864795add
Todd Libby <toddlibby@protonmail.com>
2023-07-14 11:38:43 -0700

    Google maps implementation and package additions via npm

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Loader } from "@googlemaps/js-api-loader";
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,22 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  @ViewChild("mapEl") mapEl: any;
+
+  async ngAfterViewInit(){
+    const loader = new Loader({
+      apiKey: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg",
+      version: "weekly",
+    });
+
+    loader.load().then(async () => {
+      const { Map } = await (window as any).google?.maps.importLibrary("maps");
+      let map = new Map(this.mapEl.nativeElement as HTMLElement, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+      });
+    });
+
+  }
 
 }
